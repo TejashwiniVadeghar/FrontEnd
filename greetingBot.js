b8598bafacf9c3911d467ebad29ef464229101ab
TejashwiniVadeghar <tejashwini.vadeghar@gmail.com>
2026-06-18 21:14:07 +0530

    Added changes

--- greetingBot.js
+++ greetingBot.js
@@ -12,3 +12,18 @@ console.log(botIntroduction);
 
 const botLocationSentence = "I live in " + botLocation + ".";
 console.log(botLocationSentence);
+bot = "professorBot";
+
+let nicknameIntroduction="My nickname is "+bot+".";
+console.log(nicknameIntroduction);
+bot = "awesomeTeacherBot";
+
+const newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+
+const favoriteSubject = "Computer Science";
+
+const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
+console.log(favoriteSubjectSentence);
+
+console.log("Well, it was nice to talk to you. Have a nice day!");
