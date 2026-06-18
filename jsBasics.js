ff441bf3cf14ad6f56d141c64be719bda6de90f7
TejashwiniVadeghar <tejashwini.vadeghar@gmail.com>
2026-06-18 20:52:44 +0530

    Added changes

--- jsBasics.js
+++ jsBasics.js
@@ -1,2 +1,27 @@
-let age;
-        console.log(age);
+newFunction();
+
+function newFunction() {
+    let age = 27;
+    console.log(age);
+}
+
+// concat using '+' operator
+let firstName = "John";
+let lastName = "Doe";
+
+let fullName = firstName + " " + lastName; 
+console.log(fullName); // John Doe
+
+// concat using '+=' operator
+let greeting = 'Hello';
+greeting += ', John!';
+
+console.log(greeting); // "Hello, John!"
+
+//concatenation using concat method
+let str1 = 'Hello';
+let str2 = 'World';
+
+let result = str1.concat(' ', str2); 
+console.log(result); // Hello World
+
